f63497a58c69d2d2c626b3e2d17e290e2a83e3d7
walid <walid>
2023-08-30 02:03:05 +0100

    calcule de feuille de paie

--- models/contract_inherit_model.py
+++ models/contract_inherit_model.py
@@ -9,3 +9,17 @@ class WsContractInherit(models.Model):
     training_end_date = fields.Date(string="Date de fin de formation")
     cart_allowance = fields.Float('Prime de panier')
     transport_allowance = fields.Float('Indemnité de transport')
+
+    cout_hours = fields.Monetary("Cout houres", compute='CalculCoutHours')
+    cjm = fields.Monetary('Cjm')
+    tjm = fields.Monetary("Tjm")
+
+
+    salire_net = fields.Monetary("Salaire mensuel (net)")
+    salire_anuell = fields.Monetary("Salaire annuel (brut)")
+    salire_monsuel = fields.Monetary("salaire mensuel (brut")
+
+    @api.onchange('wage')
+    def CalculCoutHours(self):
+        for rec in self:
+            rec.cout_hours = round(rec.wage / 173.33, 2)
